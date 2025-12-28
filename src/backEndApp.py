64286292bd152798ba81ab7f82e71b9288057a0c
Carl <Carl.j.paulsson@hotmail.com>
2025-12-28 15:50:53 +0100

    Post model for database

--- src/backEndApp.py
+++ src/backEndApp.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, flash
+from datetime import datetime, timezone
 from config import Config
 from forms import LoginForm
 from flask_sqlalchemy import SQLAlchemy
@@ -7,18 +8,13 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 
-
-
-
-
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
-
 # Localhost:5000
+# Routes start
 @app.route("/")
 def reRouteHome():
     return redirect("/home")
@@ -51,16 +47,36 @@ def login():
         return redirect(url_for("index"))
     return render_template("login.html", title="log in", form=form)
 
+#-- Routes end
 
+# Database  start
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(129), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
 
-def __repr__(self):
-    return f"<User {self.username}>"
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[int] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+
+    author: so.Mapped[User] = so.relationship(back_populates="posts")
+
+    def __repr__(self):
+        return f"<User {self.body}>"
+
+
+# Databse end
+
+
+
 
 if __name__=='__main__':
    app.run(debug=True) 
